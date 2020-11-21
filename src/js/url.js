12e83de914c7a6a5c04caa5f4859226d79620db2
Artem <64410376+Artiss90@users.noreply.github.com>
2020-11-21 16:20:16 +0200

    add url

---
+++ src/js/url.js
@@ -0,0 +1,5 @@
+
+
+//  https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
+
+fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=flower&page=1&per_page=12&key=19207943-ecb2269b7818ebd0e732e1fe9`)
